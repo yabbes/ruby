cf62d587f452b2b8e84be828d0fbafca0d9403c8
Tobias Machulski <yabbaspost@gmail.com>
2017-07-30 01:24:38 +0200

    suppl

--- Wagon-0/07. Code-style Gym/codestyle_gym.rb
+++ Wagon-0/07. Code-style Gym/codestyle_gym.rb
@@ -1,36 +1,41 @@
 class Player_moving
   Grid_POSITIONS = (1..100).to_a
-  
-def initialize ( position ) 
-  @position = position ; @iterations = 0; 
-raise "Position must be between 1 and 100" unless Grid_POSITIONS.include? @position
+  def initialize ( position )
+    @position = position ; @iterations = 0;
+  raise "Position must be between 1 and 100" unless Grid_POSITIONS.include? @position
   end
-  
-      def move   distanceInMeters
-@position += distanceInMeters
-  end
-  
-def play
-  if(  self.status_Player==:play   ) 
-if rand(1..100) < 50 ; move(rand(1..10))
-  else
-        move(-rand(1..10))
-end
-  end
-end
 
-def cheat; @position = 101; end
-  
-def status_Player
-    if( @position >   100 )
-      :win 
-    elsif(@position<0) ; :loose
-    else;  :play
+  def move distanceInMeters
+    @position += distanceInMeters
+  end
+
+  def play
+    if (  self.status_Player==:play   )
+      if rand(1..100) < 50
+        move(rand(1..10))
+      else
+        move(-rand(1..10))
+      end
     end
   end
-  
-  def hasWon ; status_Player  == :win ; end
-    
+
+  def cheat
+    @position = 101
+  end
+
+  def status_Player
+    if( @position >   100 )
+      :win
+    elsif(@position<0)
+      :loose
+    else
+      :play
+    end
+  end
+
+  def hasWon 
+    status_Player  == :win
+  end
 end
 
 player = Player_moving.new(50)
@@ -38,7 +43,7 @@ player.play
 puts player.status_Player == :play # true
 player.cheat
 puts player.hasWon == true
-  
+
 begin
   Player_moving.new(110)
 rescue RuntimeError => e
--- Wagon-1/01. Simple looping/simple_looping.rb
+++ Wagon-1/01. Simple looping/simple_looping.rb
@@ -1,7 +1,16 @@
 # The objective is to create a function that computes the sum of the integers from a min value to a max value
 
 def sum(min,max)
-  # your code here
+  value = 0
+  min.upto(max) do |i|
+    value += i
+  end
+  return value
+end
+
+def sum_recursive(min, max)
+   return min if min == max
+   min + sum_recursive(min + 1, max)
 end
 
 # Testing your code
@@ -10,3 +19,6 @@ min = 1
 max = 100
 sum = sum(1,100)
 puts sum == 5050 # => true
+
+sum_r = sum_recursive(1,100)
+puts sum_r == 5050 # => true?
